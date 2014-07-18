a0c24def0643f9ce804cbc52f5cb59fe89dabfae
Akikonata <66875692@qq.com>
2014-07-19 06:07:38 +0800

    Added an 3d Animate
    加上了一个简单的3d效果

--- mv.js
+++ mv.js
@@ -23,7 +23,7 @@ function enable(element, yes) {
     }
 }
 
-function play () {
+function play() {
     audio.currentTime = timing.value;
     audio.play();
     enable(playButton, false);
@@ -43,6 +43,7 @@ pauseButton.onclick = pause;
 audio.oncanplay = function() {
     enable(appleZone);
     analysis();
+    Animate3d();
     timing.max = audio.duration;
 };
 
@@ -69,3 +70,70 @@ function analysis() {
 }
 
 
+//旋转的枪
+var Animate3d = function() {
+
+};
+var WIDTH = document.documentElement.offsetWidth || 800,
+    HEIGHT = document.documentElement.clientHeight || 600;
+var scene = new THREE.Scene();
+/* 摄像头 */
+var VIEW_ANGLE = 75,
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 0.1,
+    FAR = 2000;
+var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+camera.position.set(0, 0, 100);
+scene.add(camera);
+/* 渲染器 */
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(WIDTH, HEIGHT);
+document.body.appendChild(renderer.domElement);
+var loader = new THREE.JSONLoader(true);
+loader.load("weapon.js", function(geometry) {
+    var imgloader = new THREE.TextureLoader();
+    imgloader.load('weapon.png', function(texture) {
+        /* 显示对象 */
+        var material = new THREE.MeshBasicMaterial({
+                color: '#fbf279',
+                map: texture
+            }),
+            obj;
+        obj = new THREE.Mesh(geometry, material);
+        obj.position.set(0, 0, 0);
+        scene.add(obj);
+        var dir = 1;
+        Animate3d = function() {
+            var feqData = new Uint8Array(1);
+            analyser.fftSize = 32;
+            analyser.getByteTimeDomainData(feqData);
+            obj.rotation.y += dir * feqData[0] / 10000;
+            if (obj.rotation.y >= Math.PI) {
+                obj.rotation.y = Math.PI;
+                dir = -1;
+            } else if (obj.rotation.y <= 0) {
+                obj.rotation.y = 0;
+                dir = 1;
+            }
+            obj.position.y = (feqData[0] - 128) * 2;
+            window.setTimeout(Animate3d, 0);
+            var ballgeo = new THREE.SphereGeometry(10);
+            var ballmaterial = new THREE.MeshBasicMaterial({
+                color: '#fbf279',
+                map: texture
+            });
+            var bullet = new THREE.Mesh(ballgeo, ballmaterial);
+            var bulletAnimate = function() {
+                bullet.CR += 5;
+                bullet.position.set(bullet.CR * Math.cos(bullet.CRotation), bullet.position.y, -bullet.CR * Math.sin(bullet.CRotation));
+                window.requestAnimationFrame(bulletAnimate);
+            }
+            bullet.CR = 50;
+            bullet.CRotation = obj.rotation.y;
+            bullet.position.set(100 * Math.cos(obj.rotation.y), obj.position.y + 15, -100 * Math.sin(obj.rotation.y));
+            scene.add(bullet);
+            window.requestAnimationFrame(bulletAnimate);
+            renderer.render(scene, camera);
+        }
+    });
+});
