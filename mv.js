47ce7ab7175329a6a8afef95054554b70ccfa6a7
techird <techird@qq.com>
2014-07-19 10:38:56 +0800

    优化

--- mv.js
+++ mv.js
@@ -55,16 +55,27 @@ timing.oninput = function() {
 };
 
 var paper = new kity.Paper(document.getElementById('visualization'));
-var path = new kity.Path().stroke('red', 2);
-paper.addShape(path);
+var path = new kity.Path().fill(new kity.LinearGradientBrush().pipe(function() {
+    this.addStop(0, kity.Color.createHSL(0, 80, 50));
+    this.addStop(0.25, kity.Color.createHSL(90, 80, 50));
+    this.addStop(0.5, kity.Color.createHSL(180, 80, 50));
+    this.addStop(0.75, kity.Color.createHSL(270, 80, 50));
+    paper.addResource(this);
+}));
+paper.addShape(path.setTranslate(0, 300));
 
+var feqData;
 function analysis() {
-    var feqData = new Uint8Array(1024);
-    analyser.fftSize = 2048;
-    analyser.getByteTimeDomainData(feqData);
-    path.setPathData(['M', Array.prototype.map.call(feqData, function(value, index) {
-        return [index + 10, value];
-    })]);
+    analyser.fftSize = 128;
+    analyser.smoothingTimeConstant = 0.6;
+    analyser.maxDecibels = 100;
+    analyser.minDecibels = 0;
+    feqData = new Float32Array(analyser.frequencyBinCount);
+    analyser.getFloatFrequencyData(feqData);
+    path.setPathData(Array.prototype.map.call(feqData, function(value, index) {
+        return ['M', index * 12, 0, 
+            'v', -value, 'h', 8, 'v', value * 2, 'h', -8, 'v', -value];
+    }));
     window.requestAnimationFrame(analysis, 0);
 }
 
